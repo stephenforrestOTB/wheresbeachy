1fe5bad7bf9e7e5ae4a340758850128357cedc06
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2017-11-10 14:19:04 +0000

    added more co-ords and cancel button

--- www/mrbeach.js
+++ www/mrbeach.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	prepare_board();
+	var i = 0;
+	thelog = {};
+
 	$('#test').hide();
 
 	$('#imgMap').click(function(e) {
@@ -7,7 +11,15 @@ $(document).ready(function() {
 	    $("#test").offset({left:e.pageX,top:e.pageY});
 	    imgX = (e.pageX - offset.left);
 	    imgY = (e.pageY - offset.top);
-	    console.log(imgX+" "+imgY);
+			thelog[i] = {x1 : imgX.toFixed(0), y1: imgY.toFixed(0)}
+			// var thestring = ''
+	    // if (i >= 15) {
+			// 	for (i = 0; i < 15; i++){
+			// 		thestring += i+':{ x1:'+thelog[i]['x1']+',y1: '+thelog[i]['y1']+'},\n';
+			// 	}
+			// 	console.log(thestring);
+			// }
+			i += 1;
     });
 
   $('button').click(function(){
@@ -33,4 +45,7 @@ function whoGotClicked(who){
 			alert('That is not ' + selected.name + ', Try Again!')
 		}
 	}
+}
+function prepare_board() {
+		console.log("Board prepared");
 }
