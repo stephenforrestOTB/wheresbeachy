f229f9a99177b7e04116150f959ff3b5eb13a5c4
Liz Dalton <elizabeth.dalton@onthebeach.co.uk>
2018-01-05 13:20:51 +0000

    Code changes to date

--- platforms/android/assets/www/animate.js
+++ platforms/android/assets/www/animate.js
@@ -1,14 +1,60 @@
-function myMove() {
-    var elem = document.getElementById();
-    var pos = 0;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+var firstPin = {
+  x: 1892,
+  y: 1528,
+  width: 512,
+  height: 516,
+  i: 1892
 }
+
+var SecondPin = {
+  x: 2372,
+  y: 1212,
+  width: 256,
+  height: 288,
+  i: 2372
+}
+
+ var context = document.getElementById("myCanvas");
+ var firstPin = new Image();
+ var secondPin = new Image();
+ firstPin.src = "images/Sandcastle1.png";
+ secondPin.src = "images/Sandcastle1.png";
+ firstPin.onload = function() {
+     render();
+ };
+ secondPin.onload = function() {
+     render();
+ };
+
+ function requestNewLocation(image) {
+   if (image.x >= image.i - 200) {
+         image.x -= 20;
+   }
+ }
+
+ function render(image) {
+   var c = context.getContext("2d");
+   c.clearRect(0, 0, context.width, context.height);
+   c.drawImage(image, image.x, image.y, image.width, image.height);
+ }
+
+  function animate(image){
+    context.addEventListener('click', function(event) {
+      setInterval(function () {
+        requestNewLocation(image);
+        render(image);
+      }, 100);
+     }, false);
+   }
+
+   function whatClicked(image) {
+    posX = document.getElementById("x");
+    posY = document.getElementById("Y");
+    // if (posX >= image.x && posX =< image.x + image.width ) {
+    //   if (posY >= image.y && posY =< image.y + image.height) {
+    //         image == animate(this.image);
+    console.log(posX)
+      }
+    }
+
+   }
--- platforms/android/assets/www/mrbeach.js
+++ platforms/android/assets/www/mrbeach.js
@@ -1,83 +1,107 @@
-$(document).ready(function() {
-	prepare_board(1);
-	var i = 0;
-	thelog = {};
+	$(document).ready(function() {
+		prepare_board(1);
+		var i = 0;
+		thelog = {};
 
-	$('#test').hide();
-
-	$('myCanvas').click(function(e) {
-	    var offset = $(this).offset();
-		$("#test").show();
-	    $("#test").offset({left:e.pageX,top:e.pageY});
-	    imgX = (e.pageX - offset.left);
-	    imgY = (e.pageY - offset.top);
-			thelog[i] = {x1 : imgX.toFixed(0), y1: imgY.toFixed(0)}
-			// var thestring = ''
-	    // if (i >= 15) {
-			// 	for (i = 0; i < 15; i++){
-			// 		thestring += i+':{ x1:'+thelog[i]['x1']+',y1: '+thelog[i]['y1']+'},\n';
-			// 	}
-			// 	console.log(thestring);
-			// }
-			i += 1;
-    });
-
-  $('button').click(function(){
-        $('#test').hide(500);
-        //console.log('you clicked ' + this.id);
-        whoGotClicked(this.id);
-    })
-});
-
-
-function whoGotClicked(who){
-	var selected = {};
-	who === "btnMrBeach" ? selected = mrbeach :
-	selected = cancel;
-    //console.log("who got clicked?  " + selected.name);
-	if (selected.name === "Cancel") {
 		$('#test').hide();
-	} else {
-		if (imgX >= selected.x1 & imgX <= selected.x2 & imgY >= selected.y1 & imgY <= selected.y2){
-		    alert('You found ' + selected.name + '!');
-		    $('#' + who).hide();
+
+		$('myCanvas').click(function(e) {
+		    var offset = $(this).offset();
+			$("#test").show();
+		    $("#test").offset({left:e.pageX,top:e.pageY});
+		    imgX = (e.pageX - offset.left);
+		    imgY = (e.pageY - offset.top);
+				thelog[i] = {x1 : imgX.toFixed(0), y1: imgY.toFixed(0)}
+				// var thestring = ''
+		    // if (i >= 15) {
+				// 	for (i = 0; i < 15; i++){
+				// 		thestring += i+':{ x1:'+thelog[i]['x1']+',y1: '+thelog[i]['y1']+'},\n';
+				// 	}
+				// 	console.log(thestring);
+				// }
+				i += 1;
+	    });
+
+	  $('button').click(function(){
+	        $('#test').hide(500);
+	        //console.log('you clicked ' + this.id);
+	        whoGotClicked(this.id);
+	    })
+	});
+
+
+	function whoGotClicked(who){
+		var selected = {};
+		who === "btnMrBeach" ? selected = mrbeach :
+		selected = cancel;
+	    //console.log("who got clicked?  " + selected.name);
+		if (selected.name === "Cancel") {
+			$('#test').hide();
 		} else {
-			alert('That is not ' + selected.name + ', Try Again!')
+			if (imgX >= selected.x1 & imgX <= selected.x2 & imgY >= selected.y1 & imgY <= selected.y2){
+			    alert('You found ' + selected.name + '!');
+			    $('#' + who).hide();
+			} else {
+				alert('That is not ' + selected.name + ', Try Again!')
+			}
 		}
 	}
-}
 
-function prepare_board(level) {
-		console.log("Board preparing");
-		clutter_images = ['Sandcastle1.png', 'Sandcastle2.png']
-		mrbeach_num = Math.floor(Math.random() * beach_positions[level].length);
+	function prepare_board(level) {
+			console.log("Board preparing");
+			clutter_images = ['Sandcastle1.png', 'Sandcastle2.png']
+			mrbeach_num = Math.floor(Math.random() * beach_positions[level].length);
 
-		for (i = 0; i < beach_positions[level].length; i ++){
-			var xpos = beach_positions[level][i]['x1']
-			var ypos = beach_positions[level][i]['y1']
-			if (i == mrbeach_num){
-				var imgname = "binoculars.jpg";
-			  var mrbeach = {
-				    name: "MrBeach",
-				    x1: xpos,
-				    x2: xpos + mrbeach_size.width,
-				    y1: ypos,
-				    y2: ypos + mrbeach_size.height
+			for (i = 0; i < beach_positions[level].length; i ++){
+				var xpos = beach_positions[level][i]['x1']
+				var ypos = beach_positions[level][i]['y1']
+				if (i == mrbeach_num){
+					var imgname = "binoculars.jpg";
+				  var mrbeach = {
+					    name: "MrBeach",
+					    x1: xpos,
+					    x2: xpos + mrbeach_size.width,
+					    y1: ypos,
+					    y2: ypos + mrbeach_size.height
+					}
 				}
+				else {
+					var imgname = clutter_images[Math.floor(Math.random() * clutter_images.length)];
+				}
+				//LIZ FUNCTION ( imgname, xpos, ypos ) --> put image on screen
+
 			}
-			else {
-				var imgname = clutter_images[Math.floor(Math.random() * clutter_images.length)];
-			}
-			//LIZ FUNCTION ( imgname, xpos, ypos ) --> put image on screen
+	}
 
-		}
-}
-var context = document.getElementById("myCanvas").getContext("2d");
-
-var firstPin = new Image();
-firstPin.src = "images/Sandcastle1.png";
-
-firstPin.onload = function() {
-		context.drawImage(firstPin, 1892, 1528, 256, 288);
-		 firstPin == myMove();
-};
+//  var initialPosition = {
+// 	 x: 1892,
+// 	 y: 1528
+//  };
+// 	var imagelocation = {
+// 		x: 1892,
+// 		y: 1528
+// 	};
+// 	var context = document.getElementById("myCanvas");
+// 	var firstPin = new Image();
+// 	firstPin.src = "images/Sandcastle1.png";
+// 	firstPin.onload = function() {
+// 			render();
+// 	};
+//
+// 	function requestNewLocation() {
+// 		if (imagelocation.x >= initialPosition.x - 200) {
+// 		      imagelocation.x -= 20;
+// 	  }
+// 	}
+//
+// 	function render() {
+// 		var c = context.getContext("2d");
+// 		c.clearRect(0, 0, context.width, context.height);
+// 		c.drawImage(firstPin, imagelocation.x, imagelocation.y, 256, 288);
+// 	}
+// context.addEventListener('click', function(event) {
+// 	setInterval(function () {
+// 		requestNewLocation();
+// 		render();
+// 	}, 100);
+// }, false);
