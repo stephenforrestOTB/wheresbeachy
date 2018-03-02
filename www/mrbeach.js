e5b5a0840ea7e57b7e5878dfa0677b321a87926e
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2018-03-02 14:08:31 +0000

    WIP

--- www/mrbeach.js
+++ www/mrbeach.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
-	prepare_board(1);
-	var i = 0;
-	thelog = {};
+	prepare_board(1)
+	var i = 0
+	thelog = {}
 
-	$('#test').hide();
+	$('#test').hide()
 
-	$('#myCanvas').click(function(e) {
-	    var offset = $(this).offset();
-			$("#test").show();
-	    $("#test").offset({left:e.pageX,top:e.pageY});
-	    imgX = (e.pageX - offset.left);
-	    imgY = (e.pageY - offset.top);
-		// 	thelog[i] = {x1 : imgX.toFixed(0), y1: imgY.toFixed(0)}
-		// 	var thestring = ''
-	  //   if (i >= 500) {
-		// 		for (i = 0; i < 500; i++){
-		// 			thestring += '{ x1:'+thelog[i]['x1']+',y1: '+thelog[i]['y1']+'},\n';
-		// 		}
-		// 		console.log(thestring);
-		// 	}
-		// 	i += 1;
+	$('#beach').click(function(e) {
+	    var offset = $(this).offset()
+			$("#test").show()
+	    $("#test").offset({left:e.pageX,top:e.pageY})
+	    imgX = (e.pageX - offset.left)
+	    imgY = (e.pageY - offset.top)
+			thelog[i] = {x1 : imgX.toFixed(0), y1: imgY.toFixed(0)}
+			var thestring = ''
+	    if (i >= 500) {
+				for (i = 0; i < 500; i++){
+					thestring += '{ x1:'+thelog[i]['x1']+',y1: '+thelog[i]['y1']+'},\n'
+				}
+				console.log(thestring)
+			}
+			i += 1
 	});
 
   $('button').click(function(){
@@ -29,68 +29,66 @@ $(document).ready(function() {
     })
 });
 
-
-function whoGotClicked(who){
-	var selected = {};
-	who === "btnMrBeach" ? selected = mrbeach :
-	who === "btnBinoculars" ? selected = binoculars :
-	selected = cancel;
-    //console.log("who got clicked?  " + selected.name);
-	if (selected.name === "Cancel") {
-		$('#test').hide();
-	} else {
-		if (imgX >= selected.x1 & imgX <= selected.x2 & imgY >= selected.y1 & imgY <= selected.y2){
-		    alert('You found ' + selected.name + '!');
-		    $('#' + who).hide();
+function prepare_board(level){
+	prepare_zone('beach', level)
+}
+function whoGotClicked(whobtn){
+	who = whobtn.replace('btn', '')
+	selected = hidden_items[who]
+	if (who == 'cancel'){
+		$('#test').hide()
+	}
+	else{
+		if ( imgX >= selected.position.x1 &
+			 	 imgX <= selected.position.x2 &
+				 imgY <= selected.position.y1 &
+				 imgY >= selected.position.y2 ){
+			alert('You found '+ selected.display_name + '!')
+			$('#' + whobtn).hide();
 		} else {
-			alert('That is not ' + selected.name + ', Try Again!')
+			alert('That is not ' + selected.display_name + ', Try Again!')
 		}
 	}
 }
-
-function prepare_board(level) {
-		console.log("Board preparing");
-		clutter_images = ['Sandcastle1.png', 'Sandcastle2.png'];
-		mrbeach_num = Math.floor(Math.random() * beach_positions[level].length);
-
-		for (i = 0; i < beach_positions[level].length; i ++){
-			var xpos = beach_positions[level][i]['x1'];
-			var ypos = beach_positions[level][i]['y1'];
-			if (i == mrbeach_num){
-				var imgname = "binoculars.png";
-				var height = mrbeach_size.height;
-				var width = mrbeach_size.width;
-			  binoculars = {
-				    name: "The Beach's Binoculars",
-				    x1: xpos,
-				    x2: xpos + width,
-				    y1: ypos,
-				    y2: ypos + height
-				}
+function prepare_zone(zone, level){
+	difficulty = 2 - level
+	zone_positions = shuffle(positions[zone][level])
+	index = 0
+	hidden_items = {}
+	Object.values(special_items).forEach( function(item) {
+		random_pos = zone_positions[index]
+		width = item.width * difficulty
+		height = item.height * difficulty
+		xpos = random_pos.x1
+		ypos = random_pos.y1
+		hidden_items[item.name] = {
+			display_name: item.display_name,
+			position: {
+				x1: xpos,
+				y1: ypos,
+				x2: xpos + width,
+				y2: ypos + height
 			}
-			else {
-				var imgname = clutter_images[Math.floor(Math.random() * clutter_images.length)];
-				var height = sandcastle1_size.height;
-				var width = sandcastle1_size.width;
-			}
-			lizfunction(imgname, xpos, ypos, width, height)
-
 		}
+		lizfunction(item.imgname, xpos, ypos, width, height)
+		index ++
+	})
+	for(index; index < positions[zone][level].length; index++){
+		item = clutter_items[Math.floor(Math.random() * clutter_items.length)];
+		random_pos = zone_positions[index]
+		width = item.width * difficulty
+		height = item.height * difficulty
+		xpos = random_pos.x1
+		ypos = random_pos.y1
+		lizfunction(item.imgname, xpos, ypos, width, height)
+	}
 }
-var context = document.getElementById("myCanvas").getContext("2d");
-
-//
-// firstPin.src = "images/Sandcastle1.png";
-//
-// firstPin.onload = function() {
-// 		context.drawImage(firstPin, 1892, 1528, 256, 288);
-// 		//firstPin == myMove();
-// };
+var context = document.getElementById("beach").getContext("2d");
 
 function lizfunction(imgname, xpos, ypos, width, height){
 	var image = new Image();
 	image.src = 'images/'+imgname;
 	image.onload = function(){
-		context.drawImage(image, xpos, ypos);
+		context.drawImage(image, xpos, ypos, width, height);
 	}
 }
